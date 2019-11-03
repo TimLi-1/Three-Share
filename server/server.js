49960ea0408da8d81986edefc17c32872e213a4e
Cheng Jiang <Cheng-Jiang@users.noreply.github.com>
2019-11-03 10:17:47 -0500

    start from scratch

---
+++ server/server.js
@@ -0,0 +1,17 @@
+/*
+	server.js
+
+	@author: cheng jiang
+	@date: november 2019
+
+*/
+
+// import modules
+var http = require('http');
+var io = require('socket.io');
+
+// http
+
+
+
+// socket.io
