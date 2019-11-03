e9ef548eca9e3617da3c63dac5e5997d686b3e56
Cheng Jiang <Cheng-Jiang@users.noreply.github.com>
2019-11-03 09:48:44 -0500

    some modification

---
+++ server/workspace.js
@@ -0,0 +1,17 @@
+/*
+	workspace.js
+
+	@author: cheng jiang
+	@date: november 2019
+
+*/
+
+// load modules
+const fs = require('fs');
+const path = require('path');
+const log = require('./log.js').log;
+
+var whiteboard = require('./whiteboard.js');
+var texteditor = require('./texteditor.js');
+var chatroom = require('./chatroom.js');
+
