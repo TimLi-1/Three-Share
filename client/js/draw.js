f003cd3c9bf5fccf2fd18c99d3ce080b83624d64
Cheng Jiang <Cheng-Jiang@users.noreply.github.com>
2019-11-03 20:48:02 -0500

    finished simple shared whiteboard

---
+++ client/js/draw.js
@@ -0,0 +1,75 @@
+/*
+	draw.js
+
+	@author: cheng jiang
+	@date: november 2019
+
+*/
+
+var socket;
+
+var current = {
+	color: "#000000",
+	weight: 1
+};
+
+var weightSlider = document.getElementById("weight-slider");
+weightSlider.addEventListener("change", changeWeight, false);
+
+function changeWeight(event) {
+	current.weight = event.target.value;
+}
+
+function changeColor(jscolor) {
+	current.color = '#' + jscolor;
+}
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+
+	socket = io.connect();
+
+	socket.on("draw", (data) => {
+		stroke(data.color);
+		strokeWeight(data.weight);
+		line(data.x0, data.y0, data.x1, data.y1);
+	});
+}
+
+function draw() {
+
+}
+
+function drawLine(x0, y0, x1, y1) {
+	stroke(current.color);
+	strokeWeight(current.weight);
+	line(x0, y0, x1, y1);
+	
+	// send to server
+	console.log("Client drawing line, sending data to server");
+	socket.emit("pencildraw", {
+		x0: x0,
+		y0: y0,
+		x1: x1,
+		y1: y1,
+		color: current.color,
+		weight: current.weight
+	});
+}
+
+function mousePressed() {
+	if (mouseButton === LEFT) {
+		current.x = mouseX;
+		current.y = mouseY;
+	}
+}
+
+function mouseDragged(event) {
+	if (mouseButton === LEFT) {
+		if (event.target.tagName === "CANVAS") {
+			drawLine(current.x, current.y, mouseX, mouseY);
+			current.x = mouseX;
+			current.y = mouseY;
+		}
+	}
+}
--- server/server.js
+++ server/server.js
@@ -7,11 +7,66 @@
 */
 
 // import modules
-var http = require('http');
-var io = require('socket.io');
+var http = require("http");
+var url = require("url");
+var path = require("path");
+var fs = require("fs");
 
 // http
+var root = path.join(__dirname, '..', 'client');
+var entrypath = path.join(root, "workspace.html");
 
+var server = http.createServer(handler);
+server.listen(8080);
+console.log("Server started, listening on port 8080");
+console.log("Root path is " + root);
 
+function handler(request, response) {
+
+	var pathname = request.url;
+
+	if (pathname === "/") {
+		var filepath = entrypath;
+	}
+	else {
+		var filepath = path.join(root, pathname);
+	}
+
+	console.log("Requested file path is " + filepath);
+
+	var ext = path.extname(filepath);
+
+	var typeExt = {
+		".html": "text/html",
+		".js": "text/javascript",
+		".css": "text/css"
+	};
+
+	var contentType = typeExt[ext] || "text/plain";
+
+	fs.readFile(filepath, (err, data) => {
+		if (err) {
+			response.writeHead(500);
+			return response.end("Error loading " + pathname);
+		}
+
+		response.writeHead(200, { "Content-Type" : contentType });
+		response.end(data);
+	});
+}
 
 // socket.io
+var io = require("socket.io").listen(server);
+
+io.sockets.on("connection", (socket) => {
+	console.log("We have a new client: " + socket.id);
+
+	// When another user is drawing, receive data
+	socket.on("pencildraw", (data) => {
+		socket.broadcast.emit("draw", data);
+	});
+
+	socket.on("disconnect", () => {
+		console.log("Client has disconnected");
+	});
+});
